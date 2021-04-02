51a71546e88048dbf171970a5565951974df426b
Joe Tavinor <joetavinor@gmail.com>
2021-04-02 19:17:04 +0100

    Initial production build

--- src/config/url.js
+++ src/config/url.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:4000";
+export const baseUrl = "https://jt-dengie-takeaways-api.herokuapp.com";
 
-export const apiBaseUrl = "http://localhost:4000/api";
+export const apiBaseUrl = "https://jt-dengie-takeaways-api.herokuapp.com/api";
